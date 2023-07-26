8f9814a1f8555998c98b33f39f9de84c5b5bb96f
Danil.Nechay <lestart767@gmail.com>
2023-07-26 13:57:46 +0300

    Исправление работы

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         // Объявляем переменные для хранения начального счета и суммы пополнения
-        int Balance = 600; // Начальный счет клиента
-        int Amount = 5000; // Сумма пополнения
+        int balance = 100; // Начальный счет клиента
+        int amount = 1100; // Сумма пополнения
 
         // Проверяем, превысила ли сумма пополнения порог 1000 рублей
-        if (Amount > 1000) {
+        if (amount > 1000) {
             // Рассчитываем количество бонусных рублей
-            int bonus = (Amount / 100) - 10;
+            int bonus = (amount / 100);
 
             // Увеличиваем начальный счет на сумму пополнения и добавляем бонусы
-            int finalBalance = Balance + Amount + bonus;
+            int finalBalance = balance + amount + bonus;
 
             // Выводим результат на экран
             System.out.println("Итоговый счет: " + finalBalance + " рублей");
             System.out.println("Количество бонусных рублей: " + bonus + " рублей");
         } else {
             // Если сумма пополнения не превышает 1000 рублей, бонусы не начисляются
-            int finalBalance = Balance + Amount;
+            int finalBalance = balance + amount;
 
             // Выводим результат на экран
             System.out.println("Итоговый счет: " + finalBalance + " рублей");
